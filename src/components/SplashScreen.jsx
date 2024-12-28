2cb73b05af80b94751eda995e2040b8f2c80c552
Kashif Ullah <kashifullah.dev@gmail.com>
2024-12-28 19:22:37 +0500

    fixed

--- src/components/SplashScreen.jsx
+++ src/components/SplashScreen.jsx
@@ -25,7 +25,7 @@ const SplashScreen = () => {
 
     return (
         <div className="min-h-screen px-2 bg-gradient-to-r from-blue-500 to-indigo-400 flex flex-col justify-center items-center text-white text-center">
-            {!isVisible ? <img src="./splash.svg" alt="" className="w-72 animate-bounce" /> :
+            {isVisible ? <img src="./splash.svg" alt="" className="w-72 animate-bounce" /> :
                 <>
                     <h1 className="text-3xl font-bold mb-4">Welcome to the Patient Onboarding</h1>
                     <p className="text-lg mb-6">Helps doctors speed up patient check-ins.</p>
